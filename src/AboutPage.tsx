2c52f6af5fd45d6c89440e3939c949c13b1fb046
Syed Aman <syednashikaman@gmail.com>
2024-03-06 16:23:20 -0500

    useEffect, useReducer, useMemo, dispatch, and useCallback

--- src/AboutPage.tsx
+++ src/AboutPage.tsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, {useReducer, useCallback, useMemo, useRef, useEffect} from 'react';
+
+type State = {
+    count: number;
+};
+
+type Action = {
+    type: 'increment' | 'decrement';
+}
+
+function reducer(state: State, action: Action) {
+    switch (action.type) {
+        case 'increment':
+            return { count: state.count + 1 };
+        case 'decrement':
+            return { count: state.count - 1 };
+        default:
+            throw new Error();
+    }
+}
 
 function AboutPage() {
-    return <h1>About Page</h1>
+    const [state, dispatch] = useReducer(reducer, {count: 0});
+    const previousCountRef = useRef<number | undefined>();
+
+    const increment = useCallback(() => dispatch({ type: 'increment' }), []);
+    const decrement = useCallback(() => dispatch({ type: 'decrement' }), []);
+
+    const isOdd = useMemo(() => state.count % 2 === 1, [state.count]);
+    const previousCount = previousCountRef.current;
+
+    useEffect(() => {
+        previousCountRef.current = state.count;
+    })
+
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>Count: {state.count}</p>
+            <button onClick={increment}>+</button>
+            <button onClick={decrement}>-</button>
+            {isOdd ? <p>The count is odd</p> : <p>The count is even</p>}
+            {previousCount !== undefined && <p>Previous count: {previousCount}</p>}
+        </div>
+    )
 }
 
 export default AboutPage;
